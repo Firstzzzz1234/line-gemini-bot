b624cdd83c1da1a5b9d731f32a0f83e5e6dca18c
Firstzzzz1234 <100031942+Firstzzzz1234@users.noreply.github.com>
2026-05-30 18:08:14 +0700

    Update app.py

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def handle_message(event):
 
     prompt = text[3:].strip()
 
-    model = genai.GenerativeModel("gemini-2.5-flash")
+    model = genai.GenerativeModel("gemini-3.5-flash")
     response = model.generate_content(prompt)
 
     with ApiClient(configuration) as api_client:
